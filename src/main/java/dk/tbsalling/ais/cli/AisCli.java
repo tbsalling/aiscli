8315c0487476249c3f8f60547edb13fb0fe6d792
Thomas Borg Salling <tbsalling@tbsalling.dk>
2022-10-21 11:28:00 +0200

    Support -i - read from input file

--- src/main/java/dk/tbsalling/ais/cli/AisCli.java
+++ src/main/java/dk/tbsalling/ais/cli/AisCli.java
@@ -10,15 +10,24 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.logging.LogManager;
 
 public class AisCli {
 
-    public static final String OPTION_VERBOSE = "verbose";
-    public static final String OPTION_VERBOSE_ABBR = "v";
+
+    public static final String OPTION_FROMFILE = "inputfile";
+    public static final String OPTION_FROMFILE_ABBR = "i";
+    public static final String OPTION_FROMFILE_NAME = "file";
     public static final String OPTION_OUTPUT = "output";
     public static final String OPTION_OUTPUT_ABBR = "o";
     public static final String OPTION_OUTPUT_FORMAT = "format";
+    public static final String OPTION_VERBOSE = "verbose";
+    public static final String OPTION_VERBOSE_ABBR = "v";
 
     public static boolean verbose = false;
 
@@ -54,6 +63,12 @@ public class AisCli {
                 .desc("Output received messages on stdout in given format ('csv', 'json').")
                 .build()
         );
+        OPTIONS.addOption(
+                Option.builder(OPTION_FROMFILE_ABBR).longOpt(OPTION_FROMFILE)
+                        .hasArg().argName(OPTION_FROMFILE_NAME).required()
+                        .desc("Read NMEA input from given file instead of stdin")
+                        .build()
+        );
     }
 
     private static void help() {
@@ -71,14 +86,24 @@ public class AisCli {
             LogManager.getLogManager().reset();
         }
 
+        InputStream inputStream = System.in;
+        if (cmdLine.hasOption(OPTION_FROMFILE)) {
+            try {
+                inputStream = new BufferedInputStream(Files.newInputStream(Path.of(cmdLine.getOptionValue(OPTION_FROMFILE))));
+            } catch (IOException e) {
+                System.err.println("Failed to open input file: " + e.getMessage());
+                System.exit(-1);
+            }
+        }
+
         if (cmdLine.hasOption(OPTION_OUTPUT)) {
             String format = cmdLine.getOptionValue(OPTION_OUTPUT);
             switch (format) {
                 case "csv":
-                    new CsvConverter().convert(System.in, System.out);
+                    new CsvConverter().convert(inputStream, System.out);
                     break;
                 case "json":
-                    new JsonConverter().convert(System.in, System.out);
+                    new JsonConverter().convert(inputStream, System.out);
                     break;
                 default:
                     System.err.println("Unknown output format: " + format);
